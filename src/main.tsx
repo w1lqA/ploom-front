5f02fc605bb1ef14e31950df7b1d3f3c792eca94
w1lqa <mrsocrushitel46@gmail.com>
2026-04-02 21:41:11 +0300

    integrated 2 api endpoints: /api/v1/authorize-user, /api/v1/register-new-user. added new dependencies: React Query, Zustand, Sonner(toasts), axios

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router/dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { router } from '@/app/router';
 import '@/index.css';
 
