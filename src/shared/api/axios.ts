625738aecac17ee0048e0be71ef3f0504deafb67
w1lqa <mrsocrushitel46@gmail.com>
2026-05-15 22:45:57 +0300

    added baseapiurl

--- src/shared/api/axios.ts
+++ src/shared/api/axios.ts
@@ -1,5 +1,6 @@
+import { API_BASE_URL } from '@/shared/lib/config';
 import axios from 'axios';
 
 export const apiClient = axios.create({
-    baseURL: '/api/v1',
+    baseURL: `${API_BASE_URL}/api/v1`,
 });
